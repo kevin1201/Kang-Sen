8c842b7727032936ddc2419dd854ec590577a716
kevin1201 <kls1201@163.com>
2017-11-03 10:12:32 +0800

    add c = 3;

---
+++ test.cpp
@@ -0,0 +1,9 @@
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 356 bytes | 356.00 KiB/s, done.
+Total 3 (delta 1), reused 0 (delta 0)
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/kevin1201/Kang-Sen.git
+   07f0ca0..f00407d  second_table -> second_table
+➜  git-study git:(second_table) git push
+Everything up-to-date
+➜  git-study git:(second_table) git log
